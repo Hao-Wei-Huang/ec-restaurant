409345aedbca2ab3a065d428daf865e4a29bcdf4
Hao-Wei-Huang <o0211ojack@gmail.com>
2020-05-09 16:37:03 +0800

    order

--- js/all.js
+++ js/all.js
@@ -1,6 +1,17 @@
 $(document).ready(function() {
+    // index
     $('.m-menu').click(function(e) {
         e.preventDefault();
         $('.navbar-header-menu').toggleClass("jq-show-menu");
+    });
+    // order
+    $('.hearticon').click(function(e) {
+        e.preventDefault();
+        $(this).find('.fa-heart').toggleClass("far");
+        $(this).find('.fa-heart').toggleClass("fas");
+    });
+    $('.m-order-nav-menu').click(function(e) {
+        e.preventDefault();
+        $('.order-nav-menu').slideToggle();
     });
 });
